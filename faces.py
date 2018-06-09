1f8e79f3ed53659ba584d2f7c6c87004dac909bc
Christian Otoo <christianotoo@gmail.com>
2018-06-09 23:32:48 +0000

    initial commit

---
+++ faces.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+import os.path
+import  glob
+# print "Brought together by theChristian"
+
+global ImagesWithFaces
+# global url_Images
+
+
+def identifyFace(imgName):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # load the required trained XML classifiers
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    img = cv2.imread(imgName)  # reading image from source
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # converting image to gray scale
+
+    # detecting faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        if True:
+            os.path.join(ImagesWithFaces, img)
+        else:
+            print "not an image"
+        # detecting eyeballs
+        for (ex, ey, ew, eh) in eyes:
+             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+
+img_dir = "url_Images"
+data_path = os.path.join(img_dir, '*g')
+files = glob.glob(data_path)
+# data = []
+for f1 in files:
+    identifyFace(f1)
---
+++ image.py
@@ -0,0 +1,76 @@
+import urllib2
+import cv2
+import os
+import os.path
+from os.path import basename
+from urlparse import urlsplit
+from bs4 import BeautifulSoup # for HTML parsing
+import time
+from faces import identifyFace
+import glob
+
+global urlList
+urlList = []
+global imgWithFacesDirectory
+global urlImgDirectory
+
+imgWithFacesDirectory = "ImagesWithFaces/"  # directory to save images with faces
+if not os.path.exists(imgWithFacesDirectory):  # checks if directory exist
+    os.makedirs(imgWithFacesDirectory)
+
+urlImgDirectory= "url_Images/" # directory to save images
+if not os.path.exists(urlImgDirectory): # checks if directory exist
+    os.makedirs(urlImgDirectory)
+
+# recursively download images starting from the root URL
+def downloadImages(url, level): # the root URL is level 0
+    print url
+    global urlList
+    if url in urlList: # prevent using the same URL again
+        return
+    urlList.append(url)
+    try:
+        urlContent = urllib2.urlopen(url).read()
+    except:
+        return
+
+    soup = BeautifulSoup(''.join(urlContent))
+    # find and download all images
+    imgTags = soup.findAll('img')
+    for imgTag in imgTags:
+        imgUrl = imgTag['src']
+        try:
+            imgData = urllib2.urlopen(imgUrl).read()
+            fileName = os.path.join(urlImgDirectory, basename(urlsplit(imgUrl)[2]))  # pointing to saving directory
+            output = open(fileName, 'wb')
+
+            output.write(imgData)
+            output.close()
+        except:
+            pass
+
+    # if there are links on the webpage then recursively repeat
+    if level > 0:
+        linkTags = soup.findAll('a')
+        if len(linkTags) > 0:
+            for linkTag in linkTags:
+                try:
+                    linkUrl = linkTag['href']
+                    downloadImages(linkUrl, level - 1)
+                except:
+                    pass
+
+    data_path = os.path.join(urlImgDirectory, '*g')
+    files = glob.glob(data_path)
+    for f1 in files:
+        identifyFace(f1)
+# main
+print " "
+print ("Images downloaded would be in the working directory")
+print "E.g http://yourwebaddress.com"
+time.sleep(5)
+print " "
+
+downloadImages(raw_input('Type web url and hit enter to start >> '), 1)
+
+
